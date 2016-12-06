b0bb2914d9259c4bec06f2c4ac7c1d0fecddcc9c
Elise Huard <elise@mastodonc.com>
2016-12-06 15:29:08 +0000

    Version 1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/buddy "1.2.1-SNAPSHOT"
+(defproject kixi/buddy "1.2.1"
   :description "Security library for Clojure"
   :url "https://github.com/funcool/buddy"
   :license {:name "Apache 2.0"
